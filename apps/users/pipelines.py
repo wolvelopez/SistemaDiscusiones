d39ee68a142048a73b5a338a364d189a30500327
wolvelopez <wolvelopez@gmail.com>
2014-06-12 19:46:39 +0200

    Revert "carga de la foto del avatar en facebook y twitter"
    This reverts commit 8f32396d723791d0d32844d9a8c316f8305ca693.
    
    Conflicts:
    	.idea/workspace.xml

--- apps/users/pipelines.py
+++ apps/users/pipelines.py
@@ -8,6 +8,5 @@ def get_avatar(backend, strategy, details, response, user=None, *args, **kwargs)
         url = response.get('profile_image_url', '').replace('_normal', '')
 
     if url:
-        print "La URL de la imagen es: " + url
         user.avatar = url
         user.save()
