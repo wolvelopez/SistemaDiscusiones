8f32396d723791d0d32844d9a8c316f8305ca693
wolvelopez <wolvelopez@gmail.com>
2014-06-12 19:25:42 +0200

    carga de la foto del avatar en facebook y twitter

--- SistemaDiscusiones/settings/base.py
+++ SistemaDiscusiones/settings/base.py
@@ -69,7 +69,7 @@ SOCIAL_AUTH_USER_MODEL = 'users.User'
 SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
 
 #Utilizartemos pipelines para traer la foto de usuario
-SOCIAL_AUTH_PIPELINES = (
+SOCIAL_AUTH_PIPELINE = (
     'social.pipeline.social_auth.social_details',
     'social.pipeline.social_auth.social_uid',
     'social.pipeline.social_auth.auth_allowed',
